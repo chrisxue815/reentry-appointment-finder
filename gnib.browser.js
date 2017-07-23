2da12fb94fc3612bff798194f08078f865c17148
Chris Xue <chrisxue815@gmail.com>
2017-07-23 11:16:17 +0100

    Loading libraries asynchronously

--- gnib.browser.js
+++ gnib.browser.js
@@ -5,59 +5,71 @@
 // A popup window will show up when there are available slots.
 // Note that slots are independent for typ=New and typ=Renewal.
 
+startDate = '20170723';
+endDate = '20170823';
+intervalMilliseconds = 5000;
+
 libs = [
   'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js',
 ];
 
+numLoadedLibs;
+
 for (lib of libs) {
   node = document.createElement('script');
   node.type = 'text/javascript';
   node.src = lib;
+  node.async = true;
+  node.addEventListener('load', onLibLoaded)
   document.head.appendChild(node);
 }
 
-start = moment('20170723', 'YYYYMMDD');
-end = moment('20170823', 'YYYYMMDD');
-intervalMilliseconds = 5000;
+function onLibLoaded() {
+  numLoadedLibs++;
+  if (numLoadedLibs < libs.length) return;
 
-urls = [
-  `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=New&_=1503338430497`,
-  `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=Renewal&_=1503338430497`,
-  `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Work&sbcat=All&typ=New&_=1503392385907`,
-  `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Work&sbcat=All&typ=Renewal&_=1503392385907`,
-];
+  start = moment(startDate, 'YYYYMMDD');
+  end = moment(endDate, 'YYYYMMDD');
 
-index = -1;
+  urls = [
+    `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=New&_=1503338430497`,
+    `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=Renewal&_=1503338430497`,
+    `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Work&sbcat=All&typ=New&_=1503392385907`,
+    `https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Work&sbcat=All&typ=Renewal&_=1503392385907`,
+  ];
 
-intervalId = setInterval(gnib, intervalMilliseconds);
+  index = -1;
 
-function gnib() {
-  console.log('Running');
-  index++;
-  url = urls[index % urls.length];
+  intervalId = setInterval(gnib, intervalMilliseconds);
 
-  $.getJSON(url)
-    .done(json => {
-      if (json.slots && json.slots.length > 0 && json.slots !== '[]') {
-        for (let slot of json.slots) {
-          let time = slot.time
-            ? moment(slot.time, 'DD MMMM YYYY - HH:mm')
-            : moment(slot, 'DD/MM/YYYY');
+  function gnib() {
+    console.log('Running');
+    index++;
+    url = urls[index % urls.length];
 
-          if (time >= start && time <= end) {
-            msg = url + '\n\n' + JSON.stringify(json);
-            console.log(msg);
-            alert(msg);
-            clearInterval(intervalId);
+    $.getJSON(url)
+      .done(json => {
+        if (json.slots && json.slots.length > 0 && json.slots !== '[]') {
+          for (let slot of json.slots) {
+            let time = slot.time
+              ? moment(slot.time, 'DD MMMM YYYY - HH:mm')
+              : moment(slot, 'DD/MM/YYYY');
+
+            if (time >= start && time <= end) {
+              msg = url + '\n\n' + JSON.stringify(json);
+              console.log(msg);
+              alert(msg);
+              clearInterval(intervalId);
+            }
           }
         }
-      }
-    })
-    .fail((jqxhr, textStatus, error) => {
-      err = 'Request Failed: ' + textStatus + ', ' + error + '\n\n' + url;
-      console.log(err);
-      alert(err);
-      clearInterval(intervalId);
-    });
+      })
+      .fail((jqxhr, textStatus, error) => {
+        err = 'Request Failed: ' + textStatus + ', ' + error + '\n\n' + url;
+        console.error(err);
+        alert(err);
+        clearInterval(intervalId);
+      });
+  }
 }
--- reentry.browser.js
+++ reentry.browser.js
@@ -4,55 +4,67 @@
 // After a few seconds if "Running" shows up and the number on the left increments over time, it means it's working.
 // A popup window will show up when there are available slots.
 
+startDate = '20170723';
+endDate = '20170823';
+intervalMilliseconds = 3000;
+
 libs = [
   'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js',
 ];
 
+numLoadedLibs = 0;
+
 for (lib of libs) {
   node = document.createElement('script');
   node.type = 'text/javascript';
   node.src = lib;
+  node.async = true
+  node.addEventListener('load', onLibLoaded)
   document.head.appendChild(node);
 }
 
-start = moment('20170826', 'YYYYMMDD');
-end = moment('20171026', 'YYYYMMDD');
-intervalMilliseconds = 3000;
+function onLibLoaded() {
+  numLoadedLibs++;
+  if (numLoadedLibs < libs.length) return;
 
-weekdays = Array.from(getWeekdays(start, end));
-index = -1;
-intervalId = setInterval(gnib, intervalMilliseconds);
+  start = moment(startDate, 'YYYYMMDD');
+  end = moment(endDate, 'YYYYMMDD');
 
-function gnib() {
-  console.log('Running');
-  index++;
-  weekday = weekdays[index % weekdays.length];
-  dateStr = moment(weekday).format('DD/MM/YYYY')
-  url = `https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt=${dateStr}&type=I&num=1&_=1480154930796`;
+  weekdays = Array.from(getWeekdays(start, end));
+  index = -1;
+  intervalId = setInterval(gnib, intervalMilliseconds);
 
-  $.getJSON(url)
-    .done(json => {
-      if (json.empty !== 'TRUE') {
-        msg = url + '\n\n' + JSON.stringify(json);
-        console.log(msg);
-        alert(msg);
+  function gnib() {
+    console.log('Running');
+    index++;
+    weekday = weekdays[index % weekdays.length];
+    dateStr = moment(weekday).format('DD/MM/YYYY')
+    url = `https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getApps4DT)?openagent&dt=${dateStr}&type=I&num=1&_=1480154930796`;
+
+    $.getJSON(url)
+      .done(json => {
+        if (json.empty !== 'TRUE') {
+          msg = url + '\n\n' + JSON.stringify(json);
+          console.log(msg);
+          alert(msg);
+          clearInterval(intervalId);
+        }
+      })
+      .fail((jqxhr, textStatus, error) => {
+        err = 'Request Failed: ' + textStatus + ', ' + error + '\n\n' + url;
+        console.error(err);
+        alert(err);
         clearInterval(intervalId);
-      }
-    })
-    .fail((jqxhr, textStatus, error) => {
-      err = 'Request Failed: ' + textStatus + ', ' + error + '\n\n' + url;
-      console.log(err);
-      alert(err);
-      clearInterval(intervalId);
-    });
-}
+      });
+  }
 
-function* getWeekdays(start, end) {
-  for (let date = new Date(start); date <= end; date.setDate(date.getDate() + 1)) {
-    let day = date.getDay();
-    if (day != 0 && day != 6) {
-      yield new Date(date);
+  function* getWeekdays(start, end) {
+    for (let date = new Date(start); date <= end; date.setDate(date.getDate() + 1)) {
+      let day = date.getDay();
+      if (day != 0 && day != 6) {
+        yield new Date(date);
+      }
     }
   }
 }
